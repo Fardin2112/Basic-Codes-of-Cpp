81ad22c38fbbba2b3860a12bf40531052d6834d6
Fardin2112 <fardinalam2112@gmail.com>
2023-06-02 22:32:43 +0530

    update

--- 2D_vector.cpp/input_output.cpp
+++ 2D_vector.cpp/input_output.cpp
@@ -3,9 +3,9 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    int n;
-    int m;
-    int ele1,element2;
+    int n=3;
+    int m=2;
+    int ele1=0,element2=0;
     vector <vector<int>> vec;
     for(int i=0;i<n;i++){
         for(int j=0;j<m;j++){
@@ -13,7 +13,6 @@ int main(){
             vec.push_back(ele1);
         }
     }
-
     for(int i=0;i<3;i++){
         for(int j=0;j<3;j++){
             cout<<vec[i][j];
---
+++ hasingmap.cpp/map.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    unordered_map<char,int>mp{
+        {'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000}};
+
+        cout<<mp['D'];
+
+    return 0;
+}
